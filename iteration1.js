68988059b1ae506ac27e60cd88a74260deb08629
pauCM-es <costamingarro@gmail.com>
2022-12-17 21:52:55 +0100

    iteration 1

---
+++ iteration1.js
@@ -0,0 +1,13 @@
+//**Iteración #1: Buscar el máximo**
+
+//? Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+  if (numberOne > numberTwo) {
+    return numberOne
+  } else {
+    return numberTwo
+  }
+}
+
+console.log (sum(465,432))
